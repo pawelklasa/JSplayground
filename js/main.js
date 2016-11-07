6456f307811d91eabf8c5c36334d520cc0ec37ab
Pawel Klasa <pawel.klasa@mac000181.opaltelecom.com>
2016-11-07 13:59:36 +0000

    adding project files

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var birthday = "06-04-1982";
+var city = " Bytow"
+
+function myFunction () {
+	//document.write(birthday + city);
+	document.getElementById('p1').innerHTML = "The text has changed";
+}
+
+
+
+var a = 1;
+while ( a < 10 ) { //while is a loop
+	console.log(a);
+	a++;  //add 1 to a
+}
